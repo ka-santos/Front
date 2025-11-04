584d2b02951d377b3d4a9b37d8d69bcbd0de586d
ka-santos <k.carvalho@aluno.ifsp.edu.br>
2025-11-04 15:12:16 -0300

    login no header

--- front/src/pages/Signin/index.jsx
+++ front/src/pages/Signin/index.jsx
@@ -1,101 +1,91 @@
-import "./signin.css";
+import './Signin.css';
 import { FcGoogle } from "react-icons/fc";
-import { FaFacebookF } from "react-icons/fa";
-import { useState } from "react";
-import { signInWithGooglePopup } from "../../firebase";
+import { FaFacebook } from "react-icons/fa";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { signInWithGooglePopup } from '../../firebase'; // 
+import { useNavigate } from 'react-router-dom';
+import { doLogin } from '../../lib/AuthHandler';
+import { useAuth } from '../../contexts/AuthContext';
 
-export default function signin() {
 
-  const [loading, setLoading] = useState(false);
-  const [user, setUser] = useState(null);
-  const [error, setError] = useState(null);
+export default function Signin() {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+    const { setLogged, setUser } = useAuth();
 
-  async function handleGoogleSignIn(){
-    setLoading(true);
-    setError(null);
+    async function handleGoogleSignIn() {
+        setLoading(true);
+        setError(null);
+        try {
+            const result = await signInWithGooglePopup();
 
-    try {
-      const userObj = await signInWithGooglePopup();
-      setUser({
-        name: userObj.displayName,
-        email: userObj.email,
-        photoURL: userObj.photoURL,
-        uid: userObj.uid,
-      }) 
-      console.log('Usuario logado:', userObj);
-    } catch(err) {
-      console.error('Erro ao logar com o Googl', err);
-      setError('err.message' || 'Erro no login');
-    } finally{
-      setLoading(false);
+            if(!result) {
+                throw new Error('Usuário não encontrado.');
+            }
+
+            const userObj = result;
+            const token = await userObj.getIdToken();
+
+            const userData = {
+                name: userObj.displayName,
+                email: userObj.email,
+                photoURL: userObj.photoURL,
+                uid: userObj.uid
+            }
+
+            doLogin(token, userData);
+
+            setLooged(true);
+            setUser(userData);
+            console.log('Login bem-sucedido:', userData);
+
+            navigate('/')
+        } catch (err) {
+            console.error('Erro ao fazer login com Google:', err);
+            setError(err.message || 'Erro no login');
+        } finally {
+            setLoading(false);
+        }
     }
-  }
 
-  return (
-    <div className="container">
-      <div className="card">
-        <h2>Entre na sua conta e negocie com segurança!</h2>
+    return (
+        <div className="container">
+            <div className='card'>
+                <h2>Entre na sua conta e negocie com segurança!</h2>
+                <p>Acesse e aproveite uma experiência segura dentro da ROTA</p>
 
-        <p>Acesse e aproveite uma expreriência segura dentro da KSC </p>
+                <div className='socialLogin'>
+                    <button
+                        className='google'
+                        onClick={handleGoogleSignIn}
+                        disabled={loading}
+                        aria-label='Entrar com Google'
+                    >
+                        <FcGoogle size={24}/>
+                    </button>
+                    <button className='facebook'><FaFacebook size={24}/></button>
+                </div>
 
-        <div className="socialLogin">
-          <button 
-          className="google"
-          onClick={handleGoogleSignIn}
-          disabled={loading}
-          aria-label="Entrar com Google"
-          >
-            <FcGoogle size={24} />
-          </button>
+                <div className='divider'>
+                    <span>Ou continue com</span>
+                </div>
 
-          <button className="facebook">
-            <FaFacebookF size={24} />
-          </button>
-        </div>
+                <label htmlFor='email'>Email</label>
+                <input type='email' id='email' placeholder='Digite seu email'/>
 
-        <div className="divider">
-          <span>Ou conect com</span>
-        </div>
+                <button className='acessarBtn'>Acessar</button>
 
-        <label htmlFor="email">E-mail</label>
-
-        <input type="email" id="email" placeholder="Digite seu e-mail" />
-
-        <button className="acessarBtn">Acessar</button>
-
-        <p className="register">
-          Não tem uma conta? <a href="/signup">Cadastre-se</a>
-        </p>
-
-        {error && <p style={{color: 'red', marginTop: 12 }}>{error}</p>}
-
-        {user && (
-          <div className="userInfo" style={{marginTop: 16 }}>
-            <img 
-            src={user.photoURL}
-            alt={user.name}
-            style={{ width: 48, borderRadius: '50%'}}
-            />
-
-            <div>
-              <p>
-                <strong>{user.name}</strong>
-              </p>
-              <p style={{ fontSize: 12}}>
-                {user.email}
-              </p>
+                <p className='register'>
+                    Não tem uma conta? <Link to='/signup'>Cadastre-se</Link>
+                </p>
+                {error && <p style={{ color: 'red', marginTop: 12 }}>{error}</p>}
             </div>
 
-          </div>
-        )}
-
-      </div>
-
-      <p className="terms">
-        Ao continuar, você concorda com os <a href="#">Termos de Uso</a> e{" "}
-        <a href="#">Politica de Privacidade</a> da KSC e seus parceiros, e em
-        receber comunicações da KSC.
-      </p>
-    </div>
-  );
+            <p className='terms'>
+                Ao continuar, você concorda com os <a href='#'>Termos de Uso</a> e <a href='#'>Política de Privacidade</a> da Rota e seus parceiros, e em receber comunicações da Rota.
+            </p>
+        </div>
+    )
 }
